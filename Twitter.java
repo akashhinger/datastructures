96ba9110f3f9f66bdd49fcaf5f00687a331b9ff1
1021107 <akash.hinger@blueyonder.com>
2022-08-08 10:15:17 +0530

    new commit

---
+++ Twitter.java
@@ -0,0 +1,36 @@
+class Account{
+	String username;
+	String password;
+}
+class Member extends Account{
+	int memberId;
+	public int getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(int memberId) {
+		this.memberId = memberId;
+	}
+	public void registerAccount(Account account) {
+		
+	}
+	public void followUser() {
+		
+	}
+	public void postTweet() {
+		
+	}
+	public void unFollowUser() {
+			
+	}
+	public void getNewsFeed() {
+		
+	}
+}
+public class Twitter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
